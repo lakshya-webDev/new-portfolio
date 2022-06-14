53a05ec95345573829fe14b7604c4e0d5c79c54d
Lakshya Choudhary <lakshya.webdev@gmail.com>
2022-06-14 14:32:57 +0530

    new design push

---
+++ src/content_option.js
@@ -0,0 +1,208 @@
+const logotext = "Lakshya";
+const meta = {
+    title: "Lakshya Choudhary",
+    description: "I'm Lakshya Choudhary Full stack devloper, Currently working in Latitude Technolabs Pvt. Ltd., Ahembdabad.",
+};
+
+const introdata = {
+    title: "I'm Lakshya Choudhary",
+    animated: {
+        first: "I love coding",
+        second: "I code cool websites",
+        third: "Love to design best Website",
+    },
+    description: "Creative Web Designer, use to play with code to design the website for client based Projects as per the requirements",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "About Me",
+    aboutme: "An innovative web developer with 2 years and 5 months of experience in website design. As a web designer, my objective is to make a positive impact on clients, co-workers, and the Internet using my skills and experience to design compelling and attractive websites.",
+    basic:[
+        "Proficient in HTML, CSS,Bootstrap, Material UI, Tailwind CSS and other working with libraries & snippets , scripting     languages such as PHP (Laravel), CMS WordPress & JavaScript(jQuery), React JS ",
+        "Knowledge of Git & CLI",
+        "Basic Knowledge in Adobe Photoshop, and Figma",
+        "Removal of Malware Injections(Japaneese Keyword Spam) From website and website code cleaning."
+    ]
+};
+const worktimeline = [{
+        jobtitle: "Internship",
+        where: "Webseneor",
+        date: "April 2019 - October 2019",
+    },
+    {
+        jobtitle: "Junior php & WordPress Developer",
+        where: "D'arteweb",
+        date: "October 2019 - May 2020",
+    },
+    {
+        jobtitle: "Full Stack Developer",
+        where: "Latitude Technolabs",
+        date: "2021-working",
+    },
+];
+const education = [{
+    edu: "School",
+    where: "St. Anthony's Sr. Sec. School , Udaipur-313001(Rajasthan)",
+    timeline: "2016",
+},
+{
+    edu: "UG",
+    where: "Mohan Lal Sukhadiya University , Udaipur-313001(Rajasthan)",
+    timeline: "2016-19",
+},
+{
+    edu: "PG",
+    where: "Mohan Lal Sukhadiya University , Udaipur-313001(Rajasthan)",
+    timeline: "2021-22",
+},
+];
+
+const skills = [{
+        name: "HTML/CSS",
+        value: 90,
+    },
+    {
+        name: "Bootstrap",
+        value: 85,
+    },
+    {
+        name: "JavaScript/jQuery",
+        value: 80,
+    },
+    {
+        name: "React Js",
+        value: 70,
+    },
+    {
+        name: "WordPress",
+        value: 85,
+    },
+    {
+        name: "Php",
+        value: 80,
+    },
+    {
+        name: "MySql",
+        value: 85,
+    }
+];
+
+const services = [{
+        title: "UI & Web Design",
+        description: "Innovative Designs According To The Client Based Requirements Through. Conversting The PSD Design Into Static/Dynamic Web Pages",
+    },
+    {
+        title: "WordPress Designs",
+        description: "WordPress Is A Free And Open-Source CMS Written In PHP And Paired With A MySQL Or MariaDB Database. Start A Blog , Open A Store , Build A Fan Base , Design A Portfolio.",
+    },
+    {
+        title: "Bootstrap Designs",
+        description: "Most Popular Front-End Toolkit For Responsive Design Using Bootstrap Helps In Build Fast, Responsive Sites.Quick Design And Customizing Responsive With Bootstrap",
+    },
+    {
+        title: "Php Development",
+        description: "Server Side Scripting Language, And A Powerful Tool For Making Dynamic And Interactive Web Pages Based On User Requirements",
+    },
+    {
+        title: "React JS",
+        description: "React Is A JS Library For Building Better UI For Single Page Applications.It's Components Make Our Work Easy For Development",
+    },
+    {
+        title: "WooCommerce",
+        description: "An Open-Source E-Commerce Plugin For WordPress. It Is Designed For Small To Large-Sized Online Merchants Using WordPress",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "Wordpress Website",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/800/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/550/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+    {
+        img: "https://picsum.photos/400/700/?grayscale",
+        desctiption: "The wisdom of life consists in the elimination of non-essentials.",
+        link: "#",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "lakshya.webdev@gmail.com",
+    YOUR_FONE: "(+91)7014-949546",
+    Address: "H-Block, S4/16 Near FCI College, BH. Trimurthy Complex Sector-14 Udaipur-313001(Raj.)",
+    YOUR_SERVICE_ID: "service_mug141k",
+    YOUR_TEMPLATE_ID: "template_2n355ui",
+    YOUR_USER_ID: "pfFFl8tMW3xutV0zD",
+};
+
+const socialprofils = {
+    github: "https://github.com/lakshya-webDev",
+    facebook: "https://www.facebook.com/lakshya.choudhary.52/",
+    linkedin: "https://linkedin.com/in/lakshya-choudhary-48a8321a0",
+    gmail:"mailto:lakshya.webdev@gmail.com",
+    whatsapp:"https://api.whatsapp.com/send?text=Can%20We%20Have%20Talk%20&phone=+917014949546"
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    education,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
